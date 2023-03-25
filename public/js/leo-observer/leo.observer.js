d7751f691a118ca897f06a4b36e32fe421dacc96
Puneet Makhija <puneet.makhija@nagarro.com>
2023-03-25 13:55:01 +0530

    Added

--- public/js/leo-observer/leo.observer.js
+++ public/js/leo-observer/leo.observer.js
@@ -280,7 +280,7 @@ var leoSessionStringKey = "leoctxsk",
             console.log("leocorerequest url getcontextsession");
 			console.log("inside a", a);
 			// adding it for debug
-			//lscache.set(leoSessionStringKey, "8005783", SESSION_CACHE_MINUTES);
+			lscache.set(leoSessionStringKey, "8005783", SESSION_CACHE_MINUTES);
 			LeoCorsRequest.get(!1, PREFIX_SESSION_INIT_URL + "?" + c + "&visid=" + a, [], function(d, k) {
 				var g = JSON.parse(k);
 				if (101 === g.status) {
